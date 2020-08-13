d60dc6565c6c38d13775a147ef545e2e1b509d38
Aimee Liang <aimeeeliang@gmail.com>
2020-08-13 03:22:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -48,7 +48,7 @@ def game_hash
         :steals => 3,
         :blocks => 8,
         :slam_dunks => 5
-      },      
+      }   
       {
         :player_name => "Jason Terry"
         :number => 31,
