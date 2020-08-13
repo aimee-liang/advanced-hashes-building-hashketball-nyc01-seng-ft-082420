e971b50dcc821369a41da868b4ec3df6070f3e8a
Aimee Liang <aimeeeliang@gmail.com>
2020-08-13 02:59:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,21 @@
 # Write your code here!
 def game_hash
   {
-    :home
-    :away
+    :home: {
+      :team_name => "Brooklyn Nets"
+      :colors => ["Black", "White"]
+      :players => [{
+        :player_name =>
+        :number =>
+        :shoe =>
+        :points =>
+        :rebounds =>
+        :assists =>
+        :steals =>
+        :blocks =>
+        :slam_dunks =>
+      }]
+    }
+    :away: {}
   }
 end
